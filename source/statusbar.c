7c9f8c9d9c72f53d3dd692c45606e8898a1e02af
PhilrocWP <simcityphil@gmail.com>
2015-08-28 14:44:57 -0400

    Added 12-hour time

--- source/statusbar.c
+++ source/statusbar.c
@@ -30,12 +30,39 @@ void drawStatusBar(bool wifiStatus, bool charging, int batteryLevel)
 {
 	u64 timeInSeconds = osGetTime() / 1000;
 	u64 dayTime = timeInSeconds % SECONDS_IN_DAY;
-	u8 hour = dayTime / SECONDS_IN_HOUR;
+	u8 24hour = dayTime / SECONDS_IN_HOUR;
+	u8 12hour
+	if(24hour < 13)
+		12hour === 24hour
+	if(24hour === 13)
+		12hour === 1
+	if(24hour === 14)
+		12hour === 2
+	if(24hour === 15)
+		12hour === 3
+	if(24hour === 16)
+		12hour === 4
+	if(24hour === 17)
+		12hour === 5
+	if(24hour === 18)
+		12hour === 6
+	if(24hour === 19)
+		12hour === 7
+	if(24hour === 20)
+		12hour === 8
+	if(24hour === 21)
+		12hour === 9
+	if(24hour === 22)
+		12hour === 10
+	if(24hour === 23)
+		12hour === 11
+	if(24hour === 24)
+		12hour === 12
 	u8 min = (dayTime % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE;
 	u8 seconds = dayTime % SECONDS_IN_MINUTE;
 
-	char timeString[9];
-	sprintf(timeString, "%02d:%02d:%02d", hour, min, seconds);
+	char timeString[17];
+	sprintf(timeString, "%02d:%02d:%02d (%02d:%02d:%02d)", 24hour, min, seconds, 12hour, min, seconds);
 	gfxDrawText(GFX_TOP, GFX_LEFT, NULL, timeString, 240 - 18, 400 / 2 - 16);
 
 	if(wifiStatus)
